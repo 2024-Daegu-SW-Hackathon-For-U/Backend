254063be57ff3c30f4e1b259c574509308c748c4
Koo-EunSung <ku903@naver.com>
2024-09-22 02:06:49 +0900

    Feat: LikePlace 엔티티 생성

---
+++ src/main/java/com/forU/hackathon/entity/LikePlace.java
@@ -0,0 +1,21 @@
+package com.forU.hackathon.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LikePlace {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Place place;
+
+    //ToDo: 멤버 추가할 것
+}
