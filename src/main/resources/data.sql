5d859bd0dfc5d80afe120f6b831e79e606a3e6ac
SungHHo <hamtoly7@naver.com>
2024-09-22 17:25:57 +0900

    data_sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- Map 테이블에 데이터 삽입
-INSERT INTO map (id, name, type, count) VALUES (1, '팔공산', 'PARK', 0);
-INSERT INTO map (id, name, type, count) VALUES (2, 'Historical Sites', 'HISTORY', 0);
-INSERT INTO map (id, name, type, count) VALUES (3, 'Nature Trails', 'NATURE', 0);
+INSERT INTO map (id, name, type, count) VALUES (1, '데이트 코스', SHARE, 6);
+INSERT INTO map (id, name, type, count) VALUES (2, '핫플', CURATION, 2);
+INSERT INTO map (id, name, type, count) VALUES (3, 'MZ인기', SHARE, 3);
 
 -- Place 테이블에 데이터 삽입
 INSERT INTO place (id, name, description) VALUES (10821532, "대구이월드", "대구 달서구 두류공원로 200", 35.85367145505638, 128.56471271337392);
@@ -12,8 +12,15 @@ INSERT INTO place (id, name, description) VALUES (15174531, "대구앞산공원"
 INSERT INTO place (id, name, description) VALUES (14600118, "수성못", "대구 수성구 두산동 539", 35.82770180872425, 128.61727641474653);
 
 -- PlaceInMap 테이블에 데이터 삽입
-INSERT INTO place_in_map (id, map_id, place_id) VALUES (1, 1, 1); -- City Park에 Central Fountain
-INSERT INTO place_in_map (id, map_id, place_id) VALUES (2, 2, 2); -- Historical Sites에 Old Castle
-INSERT INTO place_in_map (id, map_id, place_id) VALUES (3, 3, 3); -- Nature Trails에 Mountain View Trail
-INSERT INTO place_in_map (id, map_id, place_id) VALUES (4, 1, 4); -- City Park에 Rose Garden
-INSERT INTO place_in_map (id, map_id, place_id) VALUES (5, 2, 1); -- Historical Sites에 Central Fountain
+INSERT INTO place_in_map (id, map_id, place_id) VALUES (1, 1, 10821532);
+INSERT INTO place_in_map (id, map_id, place_id) VALUES (2, 2, 10821532);
+INSERT INTO place_in_map (id, map_id, place_id) VALUES (3, 3, 10821532);
+INSERT INTO place_in_map (id, map_id, place_id) VALUES (4, 1, 27464044);
+INSERT INTO place_in_map (id, map_id, place_id) VALUES (5, 1, 11343455);
+INSERT INTO place_in_map (id, map_id, place_id) VALUES (6, 1, 14600118);
+INSERT INTO place_in_map (id, map_id, place_id) VALUES (7, 2, 14600118);
+INSERT INTO place_in_map (id, map_id, place_id) VALUES (8, 3, 14600118);
+INSERT INTO place_in_map (id, map_id, place_id) VALUES (9, 1, 15174531);
+INSERT INTO place_in_map (id, map_id, place_id) VALUES (10, 1, 14600118);
+INSERT INTO place_in_map (id, map_id, place_id) VALUES (11, 3, 14600118);
+
